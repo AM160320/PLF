ff83abb75ac940b0b7fbe0396ec6a745608b61dd
Andjela Milenkovic <mil19625@spengergasse.at>
2023-09-10 15:25:52 +0200

    Push

---
+++ src/main/java/at/spengergasse/plf/PlfApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.plf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/plf/PlfApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.plf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
